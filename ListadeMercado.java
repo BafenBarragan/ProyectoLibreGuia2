ee0302e6b5f0938277fc7b645eff1b58510f181d
Bafen Ricardo Barragán <bbarrag92725@universidadean.edu.co>
2018-08-19 10:57:17 -0500

    Versión 2.0

--- ListadeMercado.java
+++ ListadeMercado.java
@@ -3,7 +3,7 @@ import java.util.*;
  * Clase controladora del programa. 
  * 
  * @author Bafen Ricardo Barragán
- * @version 1.0
+ * @version 2.0
  */
 public class ListadeMercado
 {
@@ -23,7 +23,33 @@ public class ListadeMercado
         
         //Llamada al método mensajeBienvenida y utilza nombreUsuario. 
         miLista.mensajeBienvenida(nombreUsuario);
+        System.out.println();
         
+        //pide los productos y los precios
+        int control = 1, //variable de control para la solicitud
+        i = 0, //variable contadora
+        j = 0; 
+        ArrayList<String> listaProductos = new ArrayList<String> (); 
+        ArrayList<Integer> listaPrecios = new ArrayList <Integer> (); 
         
+        while (control == 1){
+            System.out.print("Ingrese el nombre y la descripción del producto: ");
+            listaProductos.add(i, teclado.nextLine());
+            System.out.println();
+            
+            System.out.print("Ingrese el valor del producto: "); 
+            listaPrecios.add(i, teclado.nextInt()); 
+            System.out.println();
+            
+            i ++;
+            
+            System.out.print("Si desea añadir otro producto digite 1, si no, digite 0:"); 
+            control = teclado.nextInt(); 
+            System.out.println();
+        }
+             System.out.println();
+             miLista.listaProductos(listaProductos, listaPrecios);
+             miLista.mensajeSalida(nombreUsuario);
+            
     }
-}
+    }    
--- M_ListadeMercado.java
+++ M_ListadeMercado.java
@@ -1,10 +1,11 @@
-
+import java.util.*;
+import javax.swing.*;
 /**
  * Esta es una de las partes del desarrollo del proyecto libre para la guía 2 del bloque de estudios Construcción de Software.
  * En esta clase se encuentran los métodos que se van a ejecutar directamente desde la clase ListadeMercado. 
  * 
  * @author Bafen Ricardo Barragán 
- * @version 1.0
+ * @version 2.0
  */
 public class M_ListadeMercado
 {
@@ -12,4 +13,13 @@ public class M_ListadeMercado
     public void mensajeBienvenida (String nombreUsuario){
         System.out.printf("Bienvenido al seguidor de precios para \n%s!\n", nombreUsuario);
     }
+    public void listaProductos(ArrayList <String> listaProductos, ArrayList<Integer> listaPrecios){
+        System.out.printf("Su lista final es:\n\n %10s%25s\n\n", "Producto", "Precio");
+        for (int i = 0; i < listaProductos.size(); i++)
+        System.out.printf("%s%22d\n\n", listaProductos.get(i), listaPrecios.get(i));
+    }
+    public void mensajeSalida (String nombreUsuario){
+        System.out.printf("Muchas gracias por utilizar la aplicación %s!", nombreUsuario); 
+    }
+    
 }
